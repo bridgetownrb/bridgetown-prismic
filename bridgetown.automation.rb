361832c02a1368c9141bbc94b947445d38d3c8fb
Jared White <jared@jaredwhite.com>
2021-11-10 11:19:43 -0800

    Add additional spacing to automation script

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -3,6 +3,7 @@ add_bridgetown_plugin("bridgetown-prismic")
 append_to_file "bridgetown.config.yml" do
   <<~YAML
 
+
     # Prismic config:
     prismic_repository: repo_name_here
     autoload_paths:
